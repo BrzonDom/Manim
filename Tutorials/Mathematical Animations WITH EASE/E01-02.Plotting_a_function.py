a2ca18668cd316fd2480b3838b494aa0c645500f
Dominik Brzoň <brzon.l.dominik@gmail.com>
2024-02-23 23:51:31 +0100

    Mani.Tut. E01-02 v2.0

--- Tutorials/Mathematical Animations WITH EASE/E01-02.Plotting_a_function.py
+++ Tutorials/Mathematical Animations WITH EASE/E01-02.Plotting_a_function.py
@@ -5,8 +5,9 @@ class pltExamp(Scene):
     def construct(self):
 
         ax = Axes(x_range= (-3, 3), y_range= (-3, 3))
+        curve = ax.plot(lambda x: (x+2) * x * (x-2)/2, color= RED)
 
-        self.add(ax)
+        self.add(ax, curve)
 
 """     Execution command:  
             manim -pql E01-02.Plotting_a_function.py pltExamp     """
